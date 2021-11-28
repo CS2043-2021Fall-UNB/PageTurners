ff627ec42b90d532f00be562e432333854692b30
Spencer Walton <66219736+Spenc-e@users.noreply.github.com>
2021-11-28 15:27:21 -0400

    Updated UserCommentObject
    Variables for UserCommentObject

--- src/pageturners/models/UserCommentObject.java
+++ src/pageturners/models/UserCommentObject.java
@@ -2,7 +2,7 @@ package pageturners.models;
 
 public class UserCommentObject {
     
-    public int id; //Comments ID
+    public int commentId; //Comments ID
     public String comment; //The users comment
     public UserObject user; //The user that posted the comment
     public Date commentCreated; //When the user created the comment
