de3546d01f110bab7a8a4c197c7e46d228c6687f
Stephen White <iamsilk3495@gmail.com>
2021-11-28 15:02:57 -0400

    Drop table if exists to allow for quicker development changes

--- sql/ConstructDatabase.sql
+++ sql/ConstructDatabase.sql
@@ -1,5 +1,8 @@
 /* SQL script for constructing the PageTurners database */
 
+-- Drop existing tables
+DROP TABLE IF EXISTS UserCategory;
+
 -- Create UserCategory table
 CREATE TABLE UserCategory (
     CategoryID INT NOT NULL AUTO_INCREMENT,
