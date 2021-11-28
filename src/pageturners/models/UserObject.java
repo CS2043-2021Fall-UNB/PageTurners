6c51270ccf525e07f56905296a5912ebc8db3d4f
nathanNH <90578765+nathanNH@users.noreply.github.com>
2021-11-28 14:41:31 -0400

    Update UserObject.java
    Updated UserObject

--- src/pageturners/models/UserObject.java
+++ src/pageturners/models/UserObject.java
@@ -2,65 +2,10 @@ package pageturners.models;
 import java.util.Date;
 
 public class UserObject {
-    private String username;
-    private String password;
-    private int idNum;
-    private Date accountCreated;
-    private boolean isMod;
-    private boolean isMuted;
-
-    UserObject(String username, String password, int idNum){
-      this.username = username;
-      this.password = password;
-      this.idNum = idNum;
-      accountCreated = new Date();
-      isMod = false;
-      isMuted = false;
-    }
-
-    public String getUsername(){
-      return username;
-    }
-
-    public String getPassword(){
-      return password;
-    }
-
-    public int getIdNum(){
-      return idNum;
-    }
-
-    public Date getAccountCreated(){
-      return accountCreated;
-    }
-
-    public boolean getMuteStatus(){
-      return isMuted;
-    }
-
-    public boolean changeMuteStatus(){
-      if(isMuted){
-        isMuted = false;
-      }
-      else{
-        isMuted = true;
-      }
-
-      return isMuted;
-    }
-
-    public boolean getModStatus(){
-      return isMod;
-    }
-
-    public boolean changeModStatus(){
-      if(isMod){
-        isMod = false;
-      }
-      else{
-        isMod = true;
-      }
-
-      return isMod;
-    }
+    public String username;
+    public String password;
+    public int id;
+    public Date accountCreated;
+    public boolean isMod;
+    public boolean isMuted;
 }
