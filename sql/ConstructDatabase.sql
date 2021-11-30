d0783efe1df5129c2d548d37784758d09ee4596d
nathanNH <90578765+nathanNH@users.noreply.github.com>
2021-11-30 12:55:02 -0400

    Update ConstructDatabase.sql
    Added create UserRecord table and create AdminRecord table in ConstructDatabase.sql

--- sql/ConstructDatabase.sql
+++ sql/ConstructDatabase.sql
@@ -6,3 +6,20 @@ CREATE TABLE UserCategory (
     CategoryName VARCHAR(255) NOT NULL
 );
 
+-- Create UserRecord table
+CREATE TABLE UserRecord (
+    UserID INT NOT NULL AUTO_INCREMENT,
+    UserName VARCHAR(16) NOT NULL,
+    Password VARCHAR(64) NOT NULL,
+    AccountCreated DATE,
+    IsMod BOOLEAN,
+    IsMuted BOOLEAN
+);
+
+-- Create AdminRecordTable
+CREATE TABLE AdminRecord (
+    AdminID INT NOT NULL AUTO_INCREMENT,
+    UserName VARCHAR(16) NOT NULL,
+    Password VARCHAR(64) NOT NULL,
+    AccountCreated DATE
+);
