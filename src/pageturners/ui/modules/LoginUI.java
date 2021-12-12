3fbd62ddd9583b43d5c41da5d4a32684a76d49f7
Stephen White <iamsilk3495@gmail.com>
2021-12-12 04:28:40 -0400

    Implement LoginUI and CreateAccountUI

--- src/pageturners/ui/modules/LoginUI.java
+++ src/pageturners/ui/modules/LoginUI.java
@@ -1,11 +1,14 @@
 package pageturners.ui.modules;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
 import pageturners.controls.LoginControl;
 import pageturners.models.AdminObject;
 import pageturners.models.UserObject;
@@ -14,6 +17,10 @@ import pageturners.ui.UIElement;
 public class LoginUI extends UIElement {
     
     private final LoginControl loginControl;
+
+    private TextField usernameField;
+    private PasswordField passwordField;
+    private Label outputLabel;
 
     public LoginUI(LoginControl loginControl) {
         this.loginControl = loginControl;
@@ -26,48 +33,90 @@ public class LoginUI extends UIElement {
         grid.setVgap(5);
         grid.setHgap(5);
 
+        Label headerLabel = new Label("Already have an account? Login!");
+        headerLabel.setFont(Font.font(15));
+
         Label userNameLabel = new Label("Username:");
         Label passwordLabel = new Label("Password:");
 
-        TextField userNameField = new TextField();
-        PasswordField passwordField = new PasswordField();
+        usernameField = new TextField();
+        passwordField = new PasswordField();
 
         Button loginButton = new Button("Login");
         loginButton.setMaxWidth(Double.MAX_VALUE);
 
-        grid.add(userNameLabel, 0, 0);
-        grid.add(passwordLabel, 0, 1);
-        grid.add(userNameField, 1, 0);
-        grid.add(passwordField, 1, 1);
-        grid.add(loginButton, 0, 2, 2, 1);
+        outputLabel = new Label();
+        outputLabel.setWrapText(true);
 
-        GridPane.setFillWidth(loginButton, true);
-        grid.maxWidth(Double.MAX_VALUE);
+        grid.add(headerLabel, 0, 0, 2, 1);
+        grid.add(userNameLabel, 0, 1);
+        grid.add(passwordLabel, 0, 2);
+        grid.add(usernameField, 1, 1);
+        grid.add(passwordField, 1, 2);
+        grid.add(loginButton, 0, 3, 2, 1);
+        grid.add(outputLabel, 0, 4, 2, 1);
+
+        grid.setMaxWidth(Double.MAX_VALUE);
+
+        GridPane.setHgrow(usernameField, Priority.ALWAYS);
+        GridPane.setHgrow(passwordField, Priority.ALWAYS);
+
+        usernameField.setOnAction(event -> clickUserLogin());
+        passwordField.setOnAction(event -> clickUserLogin());
+        loginButton.setOnAction(event -> clickUserLogin());
 
         show(grid);
     }
 
     private void clickUserLogin() {
-        throw new UnsupportedOperationException("Not implemented");
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        passwordField.setText("");
+
+        if (username.length() == 0) {
+            outputLabel.setText("Please enter a username.");
+            return;
+        }
+
+        if (password.length() == 0) {
+            outputLabel.setText("Please enter a password.");
+            return;
+        }
+        
+        AdminObject admin = loginControl.handleAdminLogin(username, password);
+
+        if (admin != null) {
+            displayAdminLoginSuccess(admin);
+            return;
+        }
+
+        UserObject user = loginControl.handleUserLogin(username, password);
+
+        if (user != null) {
+            displayUserLoginSuccess(user);
+            return;
+        }
+
+        displayUserLoginFailure();
     }
 
     private void displayUserLoginSuccess(UserObject user) {
-        throw new UnsupportedOperationException("Not implemented");
+        outputLabel.setText("Successfully logged in. Welcome " + user.username + "!");
     }
 
     private void displayUserLoginFailure() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
+        Alert alert = new Alert(AlertType.WARNING);
 
-    private void clickAdminLogin() {
-        throw new UnsupportedOperationException("Not implemented");
+        alert.setGraphic(null);
+        alert.setTitle("Login Failed");
+        alert.setHeaderText("Invalid Login Credentials");
+        alert.setContentText("Login failed. Please check the username and password and try again.");
+
+        alert.showAndWait();
     }
 
     private void displayAdminLoginSuccess(AdminObject admin) {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    private void displayAdminLoginFailure() {
-        throw new UnsupportedOperationException("Not implemented");
+        outputLabel.setText("Successfully logged in as admin. Welcome " + admin.username + "!");
     }
 }
