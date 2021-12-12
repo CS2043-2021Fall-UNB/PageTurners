5dec4dd15dda68b657367afe8d893ef639e3cf59
Stephen White <iamsilk3495@gmail.com>
2021-12-12 04:30:29 -0400

    Implement basic view category UI functionality

--- src/pageturners/ui/modules/ViewCategoryUI.java
+++ src/pageturners/ui/modules/ViewCategoryUI.java
@@ -1,22 +1,62 @@
 package pageturners.ui.modules;
 
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import pageturners.controls.ViewCategoryControl;
 import pageturners.models.UserCategoryObject;
 import pageturners.models.UserPostObject;
+import pageturners.ui.UIElement;
 
-public class ViewCategoryUI {
+public class ViewCategoryUI extends UIElement {
+
     private final ViewCategoryControl viewCategoryControl;
 
     public ViewCategoryUI(ViewCategoryControl viewCategoryControl) {
         this.viewCategoryControl = viewCategoryControl;
+
+        displayCategories();
     }
 
     public void displayCategories() {
-        throw new UnsupportedOperationException("Not implemented");
+        ArrayList<UserCategoryObject> categories = viewCategoryControl.handleViewCategories();
+
+        displayCategoryObjects(categories);
     }
 
-    private void displayCategoryObjects(UserCategoryObject[] categories) {
-        throw new UnsupportedOperationException("Not implemented");
+    private void displayCategoryObjects(ArrayList<UserCategoryObject> categories) {
+        VBox layout = new VBox();
+        layout.setSpacing(5);
+        //layout.setStyle("-fx-background-color: #0000FF;");
+
+        Label label = new Label("Categories:");
+        label.setFont(Font.font(20));
+        label.prefWidthProperty().bind(layout.prefWidthProperty());
+
+        layout.getChildren().clear();
+        layout.getChildren().add(label);
+
+        for (UserCategoryObject category : categories) {
+            Button button = new Button(category.categoryName);
+            button.setOnAction((event) -> onClickCategory(event, category));
+            button.prefWidthProperty().bind(layout.widthProperty().subtract(20));
+
+            layout.getChildren().add(button);
+        }
+        
+        show(layout);
+    }
+
+    private void onClickCategory(ActionEvent event, UserCategoryObject category) {
+        //label.setText("Categories: (Clicked " + category.categoryName + ")");
     }
 
     private void clickSelectCategory() {
