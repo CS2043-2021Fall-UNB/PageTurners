587ee78d3aa5d3b4059073fa632b53c7d1ffaa72
Stephen White <iamsilk3495@gmail.com>
2021-12-12 01:29:58 -0400

    Create initial LoginUI form

--- src/pageturners/ui/modules/LoginUI.java
+++ src/pageturners/ui/modules/LoginUI.java
@@ -1,19 +1,50 @@
 package pageturners.ui.modules;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import pageturners.controls.LoginControl;
 import pageturners.models.AdminObject;
 import pageturners.models.UserObject;
+import pageturners.ui.UIElement;
 
-public class LoginUI {
+public class LoginUI extends UIElement {
     
     private final LoginControl loginControl;
 
     public LoginUI(LoginControl loginControl) {
         this.loginControl = loginControl;
+
+        displayLoginForm();
     }
 
     public void displayLoginForm() {
-        throw new UnsupportedOperationException("Not implemented");
+        GridPane grid = new GridPane();
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        Label userNameLabel = new Label("Username:");
+        Label passwordLabel = new Label("Password:");
+
+        TextField userNameField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        Button loginButton = new Button("Login");
+        loginButton.setMaxWidth(Double.MAX_VALUE);
+
+        grid.add(userNameLabel, 0, 0);
+        grid.add(passwordLabel, 0, 1);
+        grid.add(userNameField, 1, 0);
+        grid.add(passwordField, 1, 1);
+        grid.add(loginButton, 0, 2, 2, 1);
+
+        GridPane.setFillWidth(loginButton, true);
+        grid.maxWidth(Double.MAX_VALUE);
+
+        show(grid);
     }
 
     private void clickUserLogin() {
