21d1fc609ff2702801e87410d1115cefa15304c7
Stephen White <iamsilk3495@gmail.com>
2021-12-13 04:02:57 -0400

    Implemented DeleteUserPostUI

--- src/pageturners/ui/modules/DeleteUserPostUI.java
+++ src/pageturners/ui/modules/DeleteUserPostUI.java
@@ -1,31 +1,51 @@
 package pageturners.ui.modules;
 
+import javafx.scene.control.Button;
+import pageturners.controls.ControlDirectory;
 import pageturners.controls.DeleteUserPostControl;
 import pageturners.models.UserPostObject;
+import pageturners.ui.UIElement;
+import pageturners.ui.helpers.AlertHelper;
+import pageturners.ui.main.MainWindowBodyUI;
 
-public class DeleteUserPostUI {
+public class DeleteUserPostUI extends UIElement {
     
     private final DeleteUserPostControl deleteUserPostControl;
+    private final MainWindowBodyUI mainWindowBodyUI;
     private final UserPostObject userPost;
 
-    public DeleteUserPostUI(DeleteUserPostControl deleteUserPostControl, UserPostObject userPost) {
-        this.deleteUserPostControl = deleteUserPostControl;
+    public DeleteUserPostUI(ControlDirectory controlDirectory, MainWindowBodyUI mainWindowBodyUI, UserPostObject userPost) {
+        deleteUserPostControl = (DeleteUserPostControl)controlDirectory.getControl(DeleteUserPostControl.class);
+        this.mainWindowBodyUI = mainWindowBodyUI;
         this.userPost = userPost;
+
+        displayDeletePostButton();
     }
 
     public void displayDeletePostButton() {
-        throw new UnsupportedOperationException("Not implemented");
+        Button deletePostButton = new Button("Delete Post");
+
+        deletePostButton.setOnAction(event -> clickDeletePost());
+
+        show(deletePostButton);
     }
 
     private void clickDeletePost() {
-        throw new UnsupportedOperationException("Not implemented");
+        UserPostObject post = deleteUserPostControl.handleDeletePost(userPost);
+
+        if (post == null) {
+            displayFailure();
+            return;
+        }
+        
+        displayConfirmation(post);
     }
 
-    private void displayConfirmation() {
-        throw new UnsupportedOperationException("Not implemented");
+    private void displayConfirmation(UserPostObject post) {
+        mainWindowBodyUI.displayPost(post.id);
     }
 
     private void displayFailure() {
-        throw new UnsupportedOperationException("Not implemented");
+        AlertHelper.showWarning("Post Deletion Failed", "An error occurred when deleting this post.");
     }
 }
