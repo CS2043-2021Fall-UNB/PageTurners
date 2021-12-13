950965859806668a53e7a8dd8a4a692d815b91ef
Stephen White <iamsilk3495@gmail.com>
2021-12-13 04:01:49 -0400

    Add UserObject to UserCommentObject

--- src/pageturners/models/UserCommentObject.java
+++ src/pageturners/models/UserCommentObject.java
@@ -5,7 +5,10 @@ import java.util.Date;
 public class UserCommentObject {
     
     public int id; //Comments ID
+    public int postId;
     public int userId; //The user that posted the comment
-    public String content; //The users comment contents
+    public String contents; //The users comment contents
     public Date commentDate; //When the user created the comment
+
+    public UserObject user;
 }
