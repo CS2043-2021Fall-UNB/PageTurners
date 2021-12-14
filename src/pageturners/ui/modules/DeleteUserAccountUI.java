4f06b28e13636fd804166085bfce604ee0cfbff4
jtran1001 <dtran@unb.ca>
2021-12-13 20:25:30 -0400

    update DeleteUserAccountUI displayDeleteConfirmation() is unfinished

--- src/pageturners/ui/modules/DeleteUserAccountUI.java
+++ src/pageturners/ui/modules/DeleteUserAccountUI.java
@@ -13,33 +13,57 @@ public class DeleteUserAccountUI extends UIElement {
     public DeleteUserAccountUI(ControlDirectory controlDirectory, UserObject userObject) {
         this.deleteUserAccountControl = (DeleteUserAccountControl)controlDirectory.getControl(DeleteUserAccountControl.class);
         this.userObject = userObject;
+
+        displayDeleteAccountButton();
     }
 
-    public void displayDeleteAccountForm() {
-        throw new UnsupportedOperationException("Not implemented");
+    public Region getNode() {
+        if (shownRegion != null) {
+            return shownRegion;
+        }
+
+        return super.getNode();
+    }
+
+    public void displayDeleteAccountButton() {
+        Button deleteAccountButton = new Button("Delete Account");
+        deleteAccountButton.setOnAction(event -> clickDeleteAccount());
+
+        shownRegion = deleteAccountButton;
     }
 
     private void clickDeleteAccount() {
-        throw new UnsupportedOperationException("Not implemented");
+        if (!AlertHelper.showYesNo("Deleting Account", "Are you sure you'd like to delete this Account?")) {
+            return;
+        }
+
+        UserObject user = deleteUserAccountControl.handleDeleteAccount(user.id);
+
+        if(user = null){
+            displayFailure();
+            return;
+        }
+
+        displayDeleteConfirmation(user);
     }
 
-    private void displayDeleteConfirmation() {
-        throw new UnsupportedOperationException("Not implemented");
+    private void displayDeleteConfirmation(UserObject user) {
+        //mainWindowBodyUI.displayPost(user.id);
     }
 
-    private void clickConfirmDelete() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    private void clickCancelDelete() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    private void displayConfirmation() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
+//    private void clickConfirmDelete() {
+//        throw new UnsupportedOperationException("Not implemented");
+//    }
+//
+//    private void clickCancelDelete() {
+//        throw new UnsupportedOperationException("Not implemented");
+//    }
+//
+//    private void displayConfirmation() {
+//        throw new UnsupportedOperationException("Not implemented");
+//    }
 
     private void displayFailure() {
-        throw new UnsupportedOperationException("Not implemented");
+        AlertHelper.showWarning("Account Deletion Failed", "An error occurred when deleting this Account.");
     }
 }
