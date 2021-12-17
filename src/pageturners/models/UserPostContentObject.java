891291d9f72da9da06d1eecbf1e5629d03e0ba59
jtran1001 <94486318+jtran1001@users.noreply.github.com>
2021-12-16 22:37:26 -0400

    Delete UserPostContentObject.java
    Delete for Phase3 submission

--- src/pageturners/models/UserPostContentObject.java
+++
@@ -1,9 +0,0 @@
-package pageturners.models;
-
-import java.util.ArrayList;
-
-public class UserPostContentObject {
-    public UserPostObject userPost;
-    public UserObject author;
-    public ArrayList<UserCommentObject> userComments;
-}
